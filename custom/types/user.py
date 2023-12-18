6fcd7bf0fb80df3aa0f904c278bd40c55cc67936
Nathnael Dereje <nathandere1357@gmail.com>
2023-12-18 16:03:58 +0300

    Fix: add created_at attribute for user which was missing and crashing the server

--- custom/types/user.py
+++ custom/types/user.py
@@ -1,22 +1,23 @@
+import datetime
 from custom.enums.user_roles import UserRole
-from typing import Tuple
+from typing import Optional, Tuple
 
 
 class User:
     def __init__(
-        self, user_id: int, full_name: str, phone_number: str, role: UserRole
+        self, user_id: int, full_name: str, phone_number: str, role: UserRole, created_at: Optional[str] = str( datetime.datetime.now())
     ) -> None:
         self.user_id = user_id
         self.phone_number = phone_number
         self.full_name = full_name
         self.role = role
+        self.created_at = created_at
 
     @staticmethod
     def from_tuple(user_tuple: Tuple) -> "User":
         """
         The properties in the tuple should be in order, user_id, phone_number, full_name, role
         """
-        # destructure the tuple input
-        user_id, full_name, phone_number, role = user_tuple
+        user_id, full_name, phone_number, role, created_at = user_tuple
 
-        return User(user_id, full_name, phone_number, role)
+        return User(user_id, full_name, phone_number, role, created_at)
